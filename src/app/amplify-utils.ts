caaa6881fb1a3af80c8ff6e8d7d34e9c1d519bed
awallace110 <andrewwallace0@gmail.com>
2025-02-16 23:39:53 -0600

    Update amplify-utils.ts

--- src/app/amplify-utils.ts
+++ src/app/amplify-utils.ts
@@ -2,7 +2,22 @@ import { generateClient } from "aws-amplify/api";
 import { Schema } from "../../amplify/data/resource";
 import { Amplify } from "aws-amplify";
 import outputs from "../../amplify_outputs.json";
+import { configureAutoTrack } from 'aws-amplify/analytics';
 
 Amplify.configure(outputs);
 
 export const amplifyClient = generateClient<Schema>();
+
+configureAutoTrack({
+  // REQUIRED, turn on/off the auto tracking
+  enable: true,
+  // REQUIRED, the event type, it's one of 'event', 'pageView' or 'session'
+  type: 'session',
+  // OPTIONAL, additional options for the tracked event.
+  options: {
+    // OPTIONAL, the attributes of the event
+    attributes: {
+      customizableField: 'attr'
+    }
+  }
+});
