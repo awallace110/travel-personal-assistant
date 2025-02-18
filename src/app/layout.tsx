404edb4d26a5a69cf393f695aac5d05355bd81a7
awallace110 <andrewwallace0@gmail.com>
2025-02-18 12:41:38 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,28 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <script>
-        (function(n,i,v,r,s,c,x,z){x=window.AwsRumClient={q:[],n:n,i:i,v:v,r:r,c:c};window[n]=function(c,p){x.q.push({c:c,p:p});};z=document.createElement('script');z.async=true;z.src=s;document.head.insertBefore(z,document.head.getElementsByTagName('script')[0]);})(
-          'cwr',
-          '96abfaba-1258-42de-9761-d99a8f5aed1f',
-          '1.0.0',
-          'us-east-1',
-          'https://client.rum.us-east-1.amazonaws.com/1.19.0/cwr.js',
-          {
-            sessionSampleRate: 1,
-            identityPoolId: "us-east-1:243843b4-2c3c-4ba7-80e5-29fe0bb4a97a",
-            endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
-            telemetries: ["performance","errors","http"],
-            allowCookies: true,
-            enableXRay: true
-          }
-        );
-      </script>
-    </head>
-    <body className={inter.className}>
-      <AuthWrapper>{children}</AuthWrapper>
-    </body>
+      <body className={inter.className}>
+        <AuthWrapper>{children}</AuthWrapper>
+      </body>
     </html>
   );
 }
